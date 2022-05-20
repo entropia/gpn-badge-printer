1ea7f09d7b09f36a5bb8458ec3230c7b08e19451
Nils Steinger <git@n-st.de>
2022-05-20 22:55:11 +0200

    Support case-sensitive ticket code matching (disabled by default)
    Resolves #7.

--- src/opt/gpn-badge-printer/backend/gpnbp/ticket/csv.py
+++ src/opt/gpn-badge-printer/backend/gpnbp/ticket/csv.py
@@ -8,10 +8,12 @@ class CSV(TicketDataSource):
     def __init__(self,
                  path: str,
                  delimiter: str = ',',
-                 ticket_code_field: str = 'ticket_code'):
+                 ticket_code_field: str = 'ticket_code',
+                 case_sensitive: bool = True):
         Path(path)
         raw_data = list()
         self.data = dict()
+        self.case_sensitive = case_sensitive
         with open(path) as f:
             reader = csv.reader(f, delimiter=delimiter)
             for row in reader:
@@ -22,8 +24,10 @@ class CSV(TicketDataSource):
                 parsed_row = dict()
                 for item in header:
                     parsed_row[item] = row[header.index(item)]
-                self.data[parsed_row[ticket_code_field]] = parsed_row
+                index = parsed_row[ticket_code_field] if self.case_sensitive else parsed_row[ticket_code_field].lower()
+                self.data[index] = parsed_row
 
     def getTicket(self, ticket_code: str) -> dict[str, str]:
-        return self.data[ticket_code]
+        index = ticket_code if self.case_sensitive else ticket_code.lower()
+        return self.data[index]
 
