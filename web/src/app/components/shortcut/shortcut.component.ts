9612732332bd820f5562d204df97512c789eecb9
Nils Steinger <git@n-st.de>
2022-05-21 19:24:56 +0200

    Make "shortcut" key buttons in preview clickable

--- web/src/app/components/shortcut/shortcut.component.ts
+++ web/src/app/components/shortcut/shortcut.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-shortcut',
@@ -7,7 +8,11 @@ import {Component, Input} from '@angular/core';
 })
 export class ShortcutComponent {
   @Input() direction!: Direction;
+  @Input() action!: () => void;
   Direction = Direction;
+
+  constructor(private router: Router) {
+  }
 }
 
 export enum Direction {
